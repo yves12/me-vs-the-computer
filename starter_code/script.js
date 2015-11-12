233a3110d139c57e2ce3885d18e1677af345d5e9
yves1 <yvesoubida@yahoo.com>
2015-11-12 23:03:39 +0000

    I made a rock, paper, scissors game

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,12 +4,43 @@ var aiPoint = 0;
 // This function returns the selection of the computer
 function getAISelection() {
     //TODO: randomly choose between 'rock', 'paper', or 'scissors'
-}
+    var things = ['rock', 'paper', 'scissors'];
+    var thing = things[Math.floor(Math.random()*things.length)];
+    return thing;
 
+}
 // This function picks the winner
 function pickWinner(userValue, aiValue) {
     //TODO: pick the correct winner: user or ai
     //TODO: Add one point for the winner
+    if (userValue == aiValue){
+       setScore(null,1);
+       return "draw";   
+    } else if(userValue == "paper" && aiValue =="rock"){
+        setScore('userPoint',1);
+        return "user";
+        
+    } else if(userValue == "scissors" && aiValue =="paper"){
+        setScore('userPoint',1);
+        return "user";
+        
+    } else if(userValue == "rock" && aiValue =="scissors"){
+        setScore('userPoint',1);   
+        return "user";
+        
+    } else if(userValue == "paper" && aiValue =="rock"){
+        setScore('aiPoint',1);    
+        return "ai";
+        
+    } else if(userValue == "scissors" && aiValue =="paper"){
+        setScore('aiPoint',1);     
+        return "ai";
+        
+    } else if(userValue == "rock" && aiValue =="scissors"){
+        setScore('aiPoint',1);
+        return "ai";
+        
+    }
 }
 
 // This function sets the scoreboard with the correct points
@@ -20,7 +51,9 @@ function setScore() {
 // This function captures the click and picks the winner
 function evaluate(evt) {
     var userValue = evt.target.getAttribute('id');
+    console.log('USER', userValue);
     var aiValue = getAISelection();
+    console.log('AI', aiValue);
 
     var winner = pickWinner(userValue, aiValue);
 
@@ -35,5 +68,9 @@ function evaluate(evt) {
 
 // This function runs on page load
 $(document).ready(function(){
-
+    console.log('HELLO WORLD');
+ $('.token').on('click', function(event){
+   console.log(event.target)
+   evaluate(event);
+    });   
 });
